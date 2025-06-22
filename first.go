ea2354f9943a2e670bd04c5f3b0c563acbb177ee
NakonechniyVitaliy <vetalpro30@gmail.com>
2025-06-22 21:41:43 +0300

    1-3 task

---
+++ first.go
@@ -0,0 +1,28 @@
+package main
+
+//  1
+//  func IsEven(n int) bool {
+//	  return n%2 == 0
+//  }
+
+//	2
+//	func Max(a int, b int) int {
+//		if a > b {
+//			return a
+//		}
+//		return b
+//	}
+
+//  3
+//  func Factorial(n int) (fact int) {
+//	  fact = 1
+//
+//	  for i := 1; n >= i; i++ {
+//	  	fact *= i
+//	  }
+//	  return
+//  }
+
+func main() {
+
+}
