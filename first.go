759b2a1b03652d716ea600cb12e57e3a5fdf237f
NakonechniyVitaliy <vetalpro30@gmail.com>
2025-06-24 23:05:29 +0300

    4-7 tasks

--- first.go
+++ first.go
@@ -23,6 +23,50 @@ package main
 //	  return
 //  }
 
+//  4
+//	func SumSlice(nums []int) (sum int) {
+//		for _, value := range nums {
+//			sum += value
+//		}
+//		return
+//	}
+
+//  5
+//	func CountVowels(s string) (count int) {
+//		vowelsArray := [5]rune{'a', 'e', 'i', 'o', 'u'}
+//		count = 0
+//
+//		for _, wordLetter := range s {
+//			for _, valueVowel := range vowelsArray {
+//				if wordLetter == valueVowel {
+//					count++
+//				}
+//			}
+//		}
+//
+//		return
+//	}
+
+//  6
+//  func IsPrime(n int) bool {
+//	  for i := n - 1; i > 1; i-- {
+//		  if n%i == 0 {
+//		  	  return false
+//		  }
+//	  }
+//	  return true
+//  }
+
+//  7
+//  func Reverse(s string) (reverseString string) {
+//
+//	  for i := len(s) - 1; i >= 0; i-- {
+//		  reverseString += string(s[i])
+//	  }
+//	  return
+//
+//  }
+
 func main() {
 
 }
