428891bdb5f8b1d83f877ef0acd713cebcb7f969
NakonechniyVitaliy <vetalpro30@gmail.com>
2025-08-21 21:42:43 +0300

    fourth-module-interfaces

---
+++ FourtModule/fourth.go
@@ -0,0 +1,34 @@
+package FourtModule
+
+import (
+	"fmt"
+	"math"
+)
+
+type Shape interface {
+	Area() float64
+}
+
+type Circle struct {
+	Radius float64
+}
+type Rectangle struct {
+	Length float64
+	Width  float64
+}
+
+func (c Circle) Area() float64 {
+	return c.Radius * c.Radius * math.Pi
+}
+
+func (c Rectangle) Area() float64 {
+	return c.Width * c.Length
+
+}
+
+func main() {
+	var c, r Shape = Circle{Radius: 4}, Rectangle{Length: 4, Width: 4}
+
+	fmt.Printf("Circle area: %f", c.Area())
+	fmt.Printf("\nRectangle area: %f", r.Area())
+}
--- ThirdModule/third.go
+++ ThirdModule/third.go
@@ -1,78 +1,21 @@
 package main
 
-import (
-	"encoding/json"
-	"fmt"
-	"os"
-	"strings"
-)
+import "fmt"
 
-// 	1
-// 	data := []byte("Hello, File!")
-// 	e := os.WriteFile("output.txt", data, 0777)
-//
-// 	if e != nil {
-//   	fmt.Println("Error create/write file!\n", e)
-// 	}
+func perimetrRectangle(firstSide, secondSide float32) (square float32) {
+	square = firstSide * secondSide
+	return
+}
 
-//  2
-//	file_data, e := os.ReadFile("output.txt")
-//
-//	if e != nil {
-//  	fmt.Println(e)
-//	}
-//
-//	fmt.Println(string(file_data))
-
-//  3
-//
-//	jsonSlice, e := json.Marshal(personSlice)
-//
-//	if e != nil {
-//		fmt.Println("error:", e)
-//	}
-//
-//	e = os.WriteFile("people.json", jsonSlice, 0644)
-//
-//	if e != nil {
-//		fmt.Println("error:", e)
-//	}
-
-//  4
-//	dataFromFile, err := os.ReadFile("people.json")
-//
-//	if err != nil {
-//		fmt.Println(err)
-//	}
-//	personSlice := []Person{}
-//	json.Unmarshal(dataFromFile, &personSlice)
-//	fmt.Println(personSlice)
-
-type Person struct {
-	Name string
-	Age  int
+func printSquare(square float32) {
+	fmt.Println(square)
 }
 
 func main() {
 
-	personFromFile, err := os.ReadFile("people.json")
+	a := float32(10)
+	b := float32(20)
 
-	if err != nil {
-		fmt.Println(err)
-	}
-
-	parsedPersons := []Person{}
-
-	err = json.Unmarshal(personFromFile, &parsedPersons)
-
-	if err != nil {
-		fmt.Println(err)
-	}
-
-	for key, _ := range parsedPersons {
-		parsedPersons[key].Age++
-	}
-
-	err = os.WriteFile("people.json", string(parsedPersons), 0644)
+	printSquare(perimetrRectangle(a, b))
 
 }
